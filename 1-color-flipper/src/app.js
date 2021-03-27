d30ec21f183d3c48265ef3c62ab23a2b0ab2fc77
Nitin Pamnani <nitin@peppo.co.in>
2021-03-27 23:02:15 +0530

    A simple color flipper project

--- 1-color-flipper/src/app.js
+++ 1-color-flipper/src/app.js
@@ -1,1 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)","#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    // get random number between 0 and 3
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    //by default it always returns between 0 and 1
+    /*
+        but we want always between 0 and 3 i.e. the length of the colors array
+        so multipy the random number by colors.length
+    */
+    return Math.floor(Math.random()*colors.length);
+}
+
--- 1-color-flipper/src/hex.js
+++ 1-color-flipper/src/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+/*hex color consists of the hasthag and then there
+  are six values. ex #f15025
+*/
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for(let iter = 0; iter < 6; iter++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
